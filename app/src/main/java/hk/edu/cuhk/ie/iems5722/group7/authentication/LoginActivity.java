be53a27e105ca3444cbe25b962d1e1979a512c2b
JadenQ <qjddyx@gmail.com>
2021-12-25 11:39:05 +0800

    loggin and registr layout

---
+++ app/src/main/java/hk/edu/cuhk/ie/iems5722/group7/authentication/LoginActivity.java
@@ -0,0 +1,16 @@
+package hk.edu.cuhk.ie.iems5722.group7.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import hk.edu.cuhk.ie.iems5722.group7.R;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
--- app/src/main/java/hk/edu/cuhk/ie/iems5722/group7/authentication/RegisterActivity.java
+++ app/src/main/java/hk/edu/cuhk/ie/iems5722/group7/authentication/RegisterActivity.java
@@ -1,14 +1,14 @@
-package hk.edu.cuhk.ie.iems5722.group7.authentification;
+package hk.edu.cuhk.ie.iems5722.group7.authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import hk.edu.cuhk.ie.iems5722.group7.R;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
 }
