05e88a30691443563b728e4bcc96f2334a025239
JadenQ <qjddyx@gmail.com>
2021-12-25 11:22:07 +0800

    loginActivity&layout

--- app/src/main/java/hk/edu/cuhk/ie/iems5722/group7/authentication/LoginActivity.java
+++
@@ -1,2 +0,0 @@
-package hk.edu.cuhk.ie.iems5722.group7.authentication;public class LoginActivity {
-}
---
+++ app/src/main/java/hk/edu/cuhk/ie/iems5722/group7/authentification/LoginActivity.java
@@ -0,0 +1,14 @@
+package hk.edu.cuhk.ie.iems5722.group7.authentification;
+
+import androidx.appcompat.app.AppCompatActivity;
+import hk.edu.cuhk.ie.iems5722.group7.R;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
